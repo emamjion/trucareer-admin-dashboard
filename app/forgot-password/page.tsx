532686459fd206e3deb2753aa5b1f109013d7539
emamjion <emamjion@gmail.com>
2025-07-22 21:24:39 +0600

    changes forget password page link

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -95,7 +95,7 @@ export default function ForgotPasswordPage() {
 
               <div className="space-y-2">
                 <Button asChild className="w-full" size="lg">
-                  <Link href="/login">
+                  <Link href="/">
                     <ArrowLeft className="mr-2 h-4 w-4" />
                     Back to Login
                   </Link>
